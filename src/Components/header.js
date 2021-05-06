6c37a13ddd004f59792b1f91c516383f53abd155
kevin <chengyu0718@hotmail.com>
2021-05-06 15:46:50 -0700

    first upload

---
+++ src/Components/header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class header extends Component {
+    render() {
+        return (
+            <div className='header'>
+                <h1 className='header-title'>剧本杀</h1>
+            </div>
+        )
+    }
+}
---
+++ src/Components/mainPage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+
+export default class mainPage extends Component {
+    render() {
+        return (
+            <div className='mainBody'>
+                <h3 className='mainBody-title'>最新测评 </h3>
+                <div className='mainBody__box'>
+                    <div className='mainBody__box__cards'>
+                    <section className='mainBody__box__cards-img'>
+                      img
+                    </section>
+                    <section className='mainBody__box__cards-text'>
+                      text
+                    </section>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
